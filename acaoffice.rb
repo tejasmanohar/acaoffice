a18c645016dc8a72632b0cc1f06ee986e23bdc12
Tejas Manohar <me@tejasmanohar.com>
2014-09-20 18:27:14 +0000

    backend

--- acaoffice.rb
+++ acaoffice.rb
@@ -12,6 +12,25 @@ configure do
   FB_Firebase = Firebase::Client.new(ENV['FB_URL'])
 end
 
+def exists?(name)
+  meetings = FB_Firebase.get('meetings').body
+  meetings.detect { |_, v| v['name'] == name }
+end
+
 get '/' do
   erb :index
+end
+
+post '/new' do
+  @name = params[:name]
+  FB_Firebase.push('meetings', { :name => @name })
+  redirect "/#{@name}"
+end
+
+get '/:name' do
+  if !(exists? params[:name])
+    redirect '/'
+  else
+    params[:name]
+  end
 end
