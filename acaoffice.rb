ddf59ea8bef5ad5a32b7ecd1a49a0c74989d052e
Tejas Manohar <me@tejasmanohar.com>
2014-09-20 22:24:31 +0000

    leggo

--- acaoffice.rb
+++ acaoffice.rb
@@ -22,9 +22,21 @@ get '/' do
 end
 
 post '/new' do
-  @name = params[:name]
-  FB_Firebase.push('meetings', { :name => @name })
-  redirect "/#{@name}"
+  unless params[:name].nil?
+    name = params[:name]
+  else
+    name = (0...50).map { ('a'..'z').to_a[rand(6)] }.join
+  end
+    meet = params[:meet]
+    FB_Firebase.push('meetings', { :name => name, :meet => meet })
+end
+
+get '/moxtra' do
+  erb :moxtra
+end
+
+get '/meet' do
+  erb :meet
 end
 
 get '/:name' do
@@ -32,5 +44,13 @@ get '/:name' do
     redirect '/'
   else
     params[:name]
+  end
+end
+
+post '/api' do
+  if !(exists? params[:name])
+    'true'
+  else
+    'false'
   end
 end
